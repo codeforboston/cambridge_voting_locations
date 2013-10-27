db84033499e82520a0953ea27dcd700ea4df8538
Liam Morley <liam@carpeliam.com>
2013-10-27 15:08:43 -0400

    add edge case handling for multiple address matches, no matches

--- app/scripts/polling_location_finder.js
+++ app/scripts/polling_location_finder.js
@@ -15,10 +15,17 @@ define(['geojson', 'json!vendor/ELECTIONS_WardsPrecincts.geojson', 'json!vendor/
             panel: document.getElementById('directions')
         });
 
+    // keep track of user precinct across calls so we can erase previous precincts if necessary
+    var userPrecinct = null;
 
     return function(coords) {
+        // if we've already drawn a user precinct, erase it
+        if (userPrecinct) {
+            userPrecinct.setMap(null);
+            userPrecinct = null;
+        }
         // find out which ward they're in using Point in Polygon
-        var userPrecinct = null, pollingLocation = null;
+        var pollingLocation = null;
         for (var i = 0; i < precincts.length; i++) {
             if (precincts[i].containsLatLng(coords)) {
                 userPrecinct = precincts[i];
@@ -29,22 +36,23 @@ define(['geojson', 'json!vendor/ELECTIONS_WardsPrecincts.geojson', 'json!vendor/
         }
         if (!userPrecinct) {
             // TODO handle what happens if they don't live in any precinct
+            document.getElementById('directions').innerHTML = "We can't find your precinct! Sorry. Try again?";
+        } else {
+            // highlight the precinct on the map
+            userPrecinct.setMap(map);
+            map.fitBounds(userPrecinct.getBounds());
+
+            // show step-by-step directions
+            var request = {
+                origin: coords,
+                destination: pollingLocation.position,
+                travelMode: google.maps.TravelMode.WALKING
+            };
+            directionsService.route(request, function(result, status) {
+                if (status === google.maps.DirectionsStatus.OK) {
+                    directionsDisplay.setDirections(result);
+                }
+            });
         }
-
-        // highlight the precinct on the map
-        userPrecinct.setMap(map);
-        map.fitBounds(userPrecinct.getBounds());
-
-        // show step-by-step directions
-        var request = {
-            origin: coords,
-            destination: pollingLocation.position,
-            travelMode: google.maps.TravelMode.WALKING
-        };
-        directionsService.route(request, function(result, status) {
-            if (status === google.maps.DirectionsStatus.OK) {
-                directionsDisplay.setDirections(result);
-            }
-        });
     };
 });
