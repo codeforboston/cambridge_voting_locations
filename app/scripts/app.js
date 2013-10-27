bd1b47ec7f7b0168883d64ffe357a3cb6bd3abdf
Liam Morley <liam@carpeliam.com>
2013-10-27 02:28:26 -0400

    clean up source
    * standardize on 4-space tabbing
    * remove underscore dependency
    * fix jshint issues

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,7 +15,7 @@ require.config({
         bootstrapTransition: '../bower_components/sass-bootstrap/js/transition',
         text: '../bower_components/requirejs-text/text',
         geojson: '../bower_components/geojson-google-maps/GeoJSON',
-        underscore: '../bower_components/underscore/underscore-min'
+        json: 'vendor/json'
     },
     shim: {
         bootstrapAffix: {
@@ -36,7 +36,7 @@ require.config({
         bootstrapDropdown: {
             deps: ['jquery']
         },
-        bootstrapModal:{
+        bootstrapModal: {
             deps: ['jquery', 'bootstrapTransition']
         },
         bootstrapPopover: {
@@ -64,27 +64,27 @@ require.config({
 });
 
 
-require(['jquery', 'polling_location_finder'], function ($, findPollingLocationFor) {
-  'use strict';
+require(['jquery', 'polling_location_finder'], function($, findPollingLocationFor) {
+    'use strict';
 
-  $('.current-location').on('click', function(e) {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function(position) {
-        var currentLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-        findPollingLocationFor(currentLocation);
-      });
-    }
-  });
-
-  $('form').on('submit', function(e) {
-    e.preventDefault(); // don't submit form
-    var address = $('#address').val();
-    var geocoder = new google.maps.Geocoder();
-
-    geocoder.geocode({ address: address }, function(results, status) {
-      // TODO if there are multiple results, default to Cambridge-specific results
-      var result = results[0];
-      findPollingLocationFor(result.geometry.location);
+    $('.current-location').on('click', function() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+                var currentLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+                findPollingLocationFor(currentLocation);
+            });
+        }
     });
-  });
+
+    $('form').on('submit', function(e) {
+        e.preventDefault(); // don't submit form
+        var address = $('#address').val();
+        var geocoder = new google.maps.Geocoder();
+
+        geocoder.geocode({ address: address }, function(results, status) {
+            // TODO if there are multiple results, default to Cambridge-specific results
+            var result = results[0];
+            findPollingLocationFor(result.geometry.location);
+        });
+    });
 });
--- app/scripts/polling_location_finder.js
+++ app/scripts/polling_location_finder.js
@@ -1,51 +1,50 @@
-define(['underscore', 'json!ELECTIONS_WardsPrecincts.geojson', 'json!ELECTIONS_PollingLocations.geojson', 'geojson'], function(_, precinctsJSON, locationsJSON, GeoJSON) {
-      'use strict';
+define(['geojson', 'json!vendor/ELECTIONS_WardsPrecincts.geojson', 'json!vendor/ELECTIONS_PollingLocations.geojson'], function(GeoJSON, precinctsJSON, locationsJSON) {
+    'use strict';
 
-  var precincts = new GeoJSON(precinctsJSON),
-      pollingLocations = new GeoJSON(locationsJSON);
+    var precincts = new GeoJSON(precinctsJSON),
+        pollingLocations = new GeoJSON(locationsJSON);
 
-  var map = new google.maps.Map(document.getElementById("map"), {
-    center: new google.maps.LatLng(42.3736, -71.1106), // Cambridge!
-    zoom: 12
-  });
-  var directionsService = new google.maps.DirectionsService(),
-      directionsDisplay = new google.maps.DirectionsRenderer({map: map, preserveViewport: true, panel: document.getElementById("directions")});
-
-
-  return function(coords) {
-    // find out which ward they're in using Point in Polygon
-    // TODO handle what happens if they don't live in any precinct
-    var userPrecinct = _(precincts).find(function (precinct) {
-      return precinct.containsLatLng(coords);
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: new google.maps.LatLng(42.3736, -71.1106), // Cambridge!
+        zoom: 12
     });
-    userPrecinct.setMap(map);
-    map.fitBounds(userPrecinct.getBounds());
+    var directionsService = new google.maps.DirectionsService(),
+        directionsDisplay = new google.maps.DirectionsRenderer({
+            map: map,
+            preserveViewport: true,
+            panel: document.getElementById('directions')
+        });
 
-    // find the polling place
-    var pollingLocation = _(pollingLocations).find(function(location) {
-      return location.geojsonProperties.W_P === userPrecinct.geojsonProperties.WardPrecinct;
-    });
 
-    // new google.maps.Marker({position: pollingLocation.position, map: map});
-    // new google.maps.Marker({position: coords, map: map});
+    return function(coords) {
+        // find out which ward they're in using Point in Polygon
+        var userPrecinct = null, pollingLocation = null;
+        for (var i = 0; i < precincts.length; i++) {
+            if (precincts[i].containsLatLng(coords)) {
+                userPrecinct = precincts[i];
+                // polling locations are stored in the same order, so we can use the same index
+                pollingLocation = pollingLocations[i];
+                break;
+            }
+        }
+        if (!userPrecinct) {
+            // TODO handle what happens if they don't live in any precinct
+        }
 
-    // show them a map with their ward highlighted, with a marker for the polling place
+        // highlight the precinct on the map
+        userPrecinct.setMap(map);
+        map.fitBounds(userPrecinct.getBounds());
 
-    // show them step-by-step directions
-    var request = {
-      origin: coords,
-      destination: pollingLocation.position,
-      travelMode: google.maps.TravelMode.WALKING
+        // show step-by-step directions
+        var request = {
+            origin: coords,
+            destination: pollingLocation.position,
+            travelMode: google.maps.TravelMode.WALKING
+        };
+        directionsService.route(request, function(result, status) {
+            if (status === google.maps.DirectionsStatus.OK) {
+                directionsDisplay.setDirections(result);
+            }
+        });
     };
-    directionsService.route(request, function(result, status) {
-      if (status == google.maps.DirectionsStatus.OK) {
-        directionsDisplay.setDirections(result);
-      }
-    });
-  }
-
-
 });
-
-
-  
