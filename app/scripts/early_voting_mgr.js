497d86e09d99629e783741f451edc7b9bf6ecfd6
Lucy Qin <lucyqin@Lucys-MacBook-Pro.local>
2016-10-28 18:24:12 -0400

    removed sidebar expansion on marker click

--- app/scripts/early_voting_mgr.js
+++ app/scripts/early_voting_mgr.js
@@ -25,15 +25,7 @@ define(
     function whenMarkerEventsHappen(eventType, marker) {
       if (eventType === 'click') {
         for (var i = 0; i < earlyVotingLocations.length; i++) {
-
-          if (marker.getPosition().equals(earlyVotingLocations[i].getGeometry().get())) {
-            
-            var collapsedToggle = Boolean(document.getElementById('#collapse-toggle'+i).getAttribute("aria-expanded"));
-
-            if (collapsedToggle) {
-              $('#collapse'+i).collapse('show');       
-            } 
-            
+          if (marker.getPosition().equals(earlyVotingLocations[i].getGeometry().get())) {            
             $el.scrollTo($('#location'+i), 800);
           }
         }
